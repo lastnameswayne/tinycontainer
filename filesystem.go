7a6ae8d7ab569500f2019c03c966513a554dd3b8
lastnameswayne <swaynedev@gmail.com>
2024-05-27 23:42:33 +0200

    hashing files

--- filesystem.go
+++ filesystem.go
@@ -10,13 +10,17 @@ import (
 	"archive/tar"
 	"bytes"
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/hanwen/go-fuse/v2/fs"
@@ -25,7 +29,8 @@ import (
 
 type HelloRoot struct {
 	fs.Inode
-	rc io.ReadCloser
+	rc     io.ReadCloser
+	KeyDir map[string]string
 }
 
 // Open
@@ -51,6 +56,7 @@ func HeaderToFileInfo(h *tar.Header) fuse.Attr {
 func (r *HelloRoot) OnAdd(ctx context.Context) {
 	//take a tarball, save filepath, hash content and save to a map
 	//use the hash as the file location. hash should be like 10 chars long
+	fmt.Println(r.rc)
 	reader := tar.NewReader(r.rc)
 	defer r.rc.Close()
 
@@ -80,7 +86,8 @@ func (r *HelloRoot) OnAdd(ctx context.Context) {
 
 		buf := bytes.NewBuffer(make([]byte, 0, header.Size))
 		io.Copy(buf, reader)
-		dir, base := filepath.Split(filepath.Clean(header.Name))
+		filepathStr := filepath.Clean(header.Name)
+		dir, _ := filepath.Split(filepathStr)
 
 		p := r.EmbeddedInode()
 		for _, comp := range strings.Split(dir, "/") {
@@ -101,20 +108,54 @@ func (r *HelloRoot) OnAdd(ctx context.Context) {
 
 		switch header.Typeflag {
 		case tar.TypeReg, tar.TypeRegA:
-			df := &fs.MemRegularFile{}
-			df.Attr = attr
-			p.AddChild(base, r.NewPersistentInode(ctx, df, fs.StableAttr{}), false)
+			h := sha1.New()
+			content := buf.Bytes()
+			h.Write(content)
+			hash := h.Sum(nil)
+			encoded := hex.EncodeToString(hash)
+			r.KeyDir[filepathStr] = encoded
+
+			df := r.NewPersistentInode(
+				ctx, &file{
+					Attr:   attr,
+					Data:   []byte("hello world"),
+					FileID: filepathStr,
+					Themap: &r.KeyDir,
+				},
+				fs.StableAttr{Ino: 0})
+
+			p.AddChild(encoded, df, false)
 		}
 	}
-
-	ch := r.NewPersistentInode(
-		ctx, &file{Data: []byte("hello world")}, fs.StableAttr{Ino: 2})
-	r.AddChild("file.txt", ch, false)
 }
 
 func (r *HelloRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	out.Mode = 0755
 	return 0
+}
+
+func (f *HelloRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	fmt.Println("CALLED OPEN ON ROOT")
+
+	// We don't return a filehandle since we don't really need
+	// one.  The file content is immutable, so hint the kernel to
+	// cache the data.
+	return nil, fuse.FOPEN_KEEP_CACHE, 0
+}
+
+func (f *HelloRoot) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	fmt.Println("root reading file", name)
+	if ch := f.GetChild(name); ch != nil {
+		//RETURNS A CHILD (not a my own file) WHICH IS WHY MY LOOKUP IS NOT BEING CALLED
+		return ch, 0
+	}
+
+	return nil, syscall.ENOENT
+}
+func (f *file) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	fmt.Println("file reading file", name)
+
+	return nil, syscall.ENOENT
 }
 
 func (f *file) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
@@ -128,21 +169,51 @@ func (f *file) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int6
 // file is a file
 type file struct {
 	fs.Inode
-	Data []byte
+	Data   []byte
+	Attr   fuse.Attr
+	FileID string
+	mu     sync.Mutex
+	Themap *map[string]string
 }
 
-func (f *file) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	fmt.Println("CALLING OPEN FILE")
-	if f.Data == nil {
+func (f *file) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	fmt.Println("CALLED OPEN")
+	hash, ok := (*f.Themap)[f.FileID]
+	if !ok {
+		fmt.Println("its not in the map")
+		//call the NFS
 	}
+	fmt.Println("FOUND HASH")
+
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if f.Data == nil {
+		rc, err := os.Open(hash)
+		if err != nil {
+			return nil, 0, syscall.EIO
+		}
+		content, err := io.ReadAll(rc)
+		if err != nil {
+			return nil, 0, syscall.EIO
+		}
+
+		f.Data = content
+	}
+
+	// We don't return a filehandle since we don't really need
+	// one.  The file content is immutable, so hint the kernel to
+	// cache the data.
 	return nil, fuse.FOPEN_KEEP_CACHE, 0
 }
 
 var _ = (fs.NodeGetattrer)((*HelloRoot)(nil))
 var _ = (fs.NodeOnAdder)((*HelloRoot)(nil))
+var _ = (fs.NodeOpener)((*HelloRoot)(nil))
 
 var _ = (fs.NodeReader)((*file)(nil))
 var _ = (fs.NodeOpener)((*file)(nil))
+var _ = (fs.NodeLookuper)((*HelloRoot)(nil))
+var _ = (fs.NodeLookuper)((*file)(nil))
 
 func main() {
 
@@ -157,7 +228,13 @@ func main() {
 	if err != nil {
 		log.Default().Printf("Command execution failed: %v", err)
 	}
-	server, err := fs.Mount(flag.Arg(0), &HelloRoot{}, opts)
+	//init root
+	f, err := os.Open("archive.tar")
+	if err != nil {
+		panic(err)
+	}
+	root := &HelloRoot{rc: f, KeyDir: map[string]string{}}
+	server, err := fs.Mount(flag.Arg(0), root, opts)
 	if err != nil {
 		log.Fatalf("Mount fail: %v\n", err)
 	}
